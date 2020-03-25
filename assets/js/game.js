53304b775a767385b6d0e253fe7615c5e08894be
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-03-25 02:10:33 -0700

    added more fight peramiters

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,32 @@
-function fight() {
-    //notifying the player.
-    window.alert("The fight has begun!");
-    //playerName input + logging it
-    var playerName = window.prompt("What is your robot's name?");
-    console.log(playerName);
-    //test messages.
-    console.log("this logs a string, good for leving youself a message");
-    //math.
-    console.log(10 + 10);
-    //combining strings and variables by making an string concatenation.
-    console.log("Our robots name is " + playerName);
-}
+
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+//loging player info
+console.log(playerName, playerHealth, playerAttack);
+
+//opponent
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+//function
+var fight = function() {
+    // alert users that they are starting the round
+    window.alert("welcome to Robot Gladiators!");
+    // Subtract the value of 'playerAttack' from the value of `enemyHealth` and use that result to update the `enemyHealth` var
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update `playerHealth` values
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to console so we know it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    )
+};
+//executing function
  fight();
