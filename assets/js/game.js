532e6b5d53eaf6c4522937cff5c148db3d6d52d7
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-03-25 00:28:25 -0700

    played with some var and log code.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("this is an alert! javaScript is running.")
+function fight() {
+    //notifying the player.
+    window.alert("The fight has begun!");
+    //playerName input + logging it
+    var playerName = window.prompt("What is your robot's name?");
+    console.log(playerName);
+    //test messages.
+    console.log("this logs a string, good for leving youself a message");
+    //math.
+    console.log(10 + 10);
+    //combining strings and variables by making an string concatenation.
+    console.log("Our robots name is " + playerName);
+}
+ fight();
